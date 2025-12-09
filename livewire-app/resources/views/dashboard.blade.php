ec65d1012de993f03234034222cfa2f62d4f332c
DarkGrimm10 <darkar132xd@gmail.com>
2025-12-09 22:01:12 +0000

    penguin iu added

--- livewire-app/resources/views/dashboard.blade.php
+++ livewire-app/resources/views/dashboard.blade.php
@@ -5,6 +5,10 @@
         </h2>
     </x-slot>
 
+    <!-- primary Button -->
+    <button type="button" class="whitespace-nowrap rounded-radius bg-primary border border-primary px-4 py-2 text-sm font-medium tracking-wide text-on-primary transition hover:opacity-75 text-center focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary active:opacity-100 active:outline-offset-0 disabled:opacity-75 disabled:cursor-not-allowed dark:bg-primary-dark dark:border-primary-dark dark:text-on-primary-dark dark:focus-visible:outline-primary-dark">Primary</button>
+
+
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
